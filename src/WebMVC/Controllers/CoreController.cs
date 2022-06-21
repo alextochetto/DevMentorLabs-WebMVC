68e1b7787a3b73b347ab93fdb57881eb5fb60e15
Alex Tochetto <alextochetto@gmail.com>
2022-06-21 18:41:42 -0300

    read file

--- src/WebMVC/Controllers/CoreController.cs
+++ src/WebMVC/Controllers/CoreController.cs
@@ -1,5 +1,9 @@
 ﻿using Core.Contract;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
 using System.Threading.Tasks;
 using Web.DTO.Core;
 using WebMVC.Models;
@@ -30,6 +34,48 @@ namespace WebMVC.Controllers
             userAddQuery.Password = userAddViewModel.Password;
             await this._user.AddUser(userAddQuery);
             return View(userAddViewModel);
+        }
+
+        public IActionResult File()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> FileUpload(List<IFormFile> files)
+        {
+            //long size = files.Sum(f => f.Length);
+
+            List<string> filePaths = new List<string>();
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    // full path to file in temp location
+                    var filePath = Path.GetTempFileName(); //we are using Temp file name just for the example. Add your own file path.
+                    filePaths.Add(filePath);
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+
+                    StringBuilder sb = new StringBuilder();
+                    using (StreamReader sr = new StreamReader(filePath, Encoding.UTF8, true))
+                    {
+                        string line;
+                        // Read and display lines from the file until the end of 
+                        // the file is reached.
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            sb.AppendLine(line);
+                        }
+                    }
+                    string allines = sb.ToString();
+                }
+            }
+            // process uploaded files
+            // Don't rely on or trust the FileName property without validation.
+            return Ok(new { count = files.Count, filePaths });
         }
     }
 }
